86c82c05f5ddcbd1f142d9aa51cd975f04ff5e40
ildarere <maxutov999@gmail.com>
2022-05-22 17:15:06 +0400

    Initial commit

---
+++ src/main/java/com/example/carcassonne/CarcassonneApplication.java
@@ -0,0 +1,13 @@
+package com.example.carcassonne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcassonneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarcassonneApplication.class, args);
+	}
+
+}
