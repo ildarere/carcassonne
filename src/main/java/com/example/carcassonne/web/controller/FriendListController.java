7344ce2790768a60346f5c8a136f1c68ce09ea1f
ildarere <maxutov999@gmail.com>
2022-05-24 13:10:20 +0400

    кондроллер для /friends

---
+++ src/main/java/com/example/carcassonne/web/controller/FriendListController.java
@@ -0,0 +1,15 @@
+package com.example.carcassonne.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FriendListController {
+    @GetMapping("/friends")
+    public String friends(Model model) {
+
+
+        return "/friends";
+    }
+}
