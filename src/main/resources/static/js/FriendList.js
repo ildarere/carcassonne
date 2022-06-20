52de868ab05c756db3c2ed0b3e46dd2ff2d232e8
Tochnoneyura <tochnoneyura@gmail.com>
2022-06-20 21:06:46 +0400

    вывод друзей

--- src/main/resources/static/js/FriendList.js
+++ src/main/resources/static/js/FriendList.js
@@ -27,12 +27,37 @@ function FriendsCallback() {
 
 	if (xhr.status === 200) {
 		let data = xhr.responseText;
-		//TODO:очистить список друзей
+		let dataJson = JSON.parse(data);
+		    let blocks = document.querySelectorAll('gameSubMain2');
+		for(let b of blocks){
+		    b.remove();
+		}
         if (data.length > 0) {
                 try {
                     let dataJson = JSON.parse(data);
                     console.log(dataJson)
-                    //TODO:добавить вывод друзей
+                    dataJson.forEach(p=>{
+                    let div = document.createElement('div')
+                    div.class.add("gameSubMain2")
+                    let img = document.createElement('img')
+                    img.class.add("imgPlus")
+                    let pict ="img/gameLogo.png"
+                    img.scr = pict;
+                    let a = document.createElement('a');
+                    a.class.add("playerName");
+                    a.innerHTML=p.name;
+                    let div2 = document.createElement('div');
+                    div2.class.add("default");
+                    div2.innerHTML=p.raiting;
+                    let div3 = document.createElement('div');
+                    div3.class.add("hiddenDelete");
+                    div3.innerHTML="Удалить"
+                    gameMain.appendChild(div);
+                    div.appendChild(img);
+                    div.appendChild(a);
+                    div.appendChild(div2);
+                    div.appendChild(div3);
+                    }
                 } catch(e) {
 
                 }
